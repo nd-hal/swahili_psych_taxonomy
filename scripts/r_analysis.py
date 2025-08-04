4de8b0763095034b995a635405325ec643613899
Kezy Oketch <79195490+Kezy-Oketch@users.noreply.github.com>
2025-08-04 15:28:32 -0400

    Update r_analysis.py

--- scripts/r_analysis.py
+++ scripts/r_analysis.py
@@ -1,6 +1,8 @@
-
 import subprocess
 
 def main():
     result = subprocess.run(["Rscript", "scripts/intersectionalBiasPlot.R"], check=True)
     print("R script completed", result.returncode)
+
+if __name__ == "__main__":
+    main()
